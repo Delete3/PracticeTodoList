640abeac9577609052c6057ae1f960a763a18efe
Delete3 <mjian827@gmail.com>
2021-04-25 17:38:50 +0800

    完成所有基本功能

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import {nanoid} from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
-import FelterButton from "./components/FilterButton"
 import FilterButton from "./components/FilterButton";
 
 const FILTER_MAP = {
@@ -11,11 +10,15 @@ const FILTER_MAP = {
   Completed: task => task.completed
 };
 
+const FILTER_NAMES = Object.keys(FILTER_MAP);//收集數組
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState('All');
   
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
       id={task.id}
       name={task.name}
@@ -25,8 +28,16 @@ function App(props) {
       deleteTask={deleteTask}
       editTask={editTask}
     />
-  )
-  );
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ))
 
   function addTask(name){
     const newTask = {id:"todo-" + nanoid(), name:name, completed:false};
@@ -66,9 +77,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
